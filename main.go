a109e9f3aea0b4ed9c2a6c515664cf8e387ddb7a
baixp <baixp@dustess.com>
2022-06-18 17:43:21 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ruixijiejie/blog-service/internal/routers"
+	"net/http"
+	"time"
+)
+
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		return
+	}
+}
